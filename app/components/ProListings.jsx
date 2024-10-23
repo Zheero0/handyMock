3212626d8b09aec635eeb4f587ea08aefddb5fd8
Zheero0 <reeco.liburd@outlook.com>
2024-10-23 01:23:23 +0100

    search bar added to directory

--- app/components/ProListings.jsx
+++ app/components/ProListings.jsx
@@ -107,6 +107,90 @@ export default function ProListings() {
           </span>
         </Link>
       </div>
+      {/* {" search inpt"} */}
+      <div className="flex flex-col justify-center mx-auto mb-4" >
+              <div className="flex justify-center items-center max-w-[1000px] w-full bg-white border border-gray-200 rounded-full shadow-md p-2 md:p-4 space-x-4">
+        {/* Job Title Input */}
+        <div className="flex flex-col rounded-full p-2 hover:bg-gray-100 transition-colors duration-200">
+          <label className="text-xs font-semibold ml-5 text-gray-600">
+            Job Title
+          </label>
+          <input
+            type="text"
+            placeholder="Search Jobs"
+
+
+            className="w-full px-4 py-2 rounded-full text-black bg-transparent focus:outline-none"
+          />
+        </div>{" "}
+        {/* Postcode Input */}
+        <div className="flex flex-col rounded-full p-2 hover:bg-gray-100 transition-colors duration-200">
+          <label className="text-xs font-semibold ml-5 text-gray-600">
+            Address
+          </label>
+          <input
+            type="text"
+            placeholder="Search Postcode"
+            className="w-full px-4 py-2 rounded-full text-black bg-transparent focus:outline-none"
+          />
+        </div>
+        {/* Location Dropdown */}
+        <div className="flex flex-col rounded-full p-2 hover:bg-gray-100 transition-colors duration-200">
+          <label className="text-xs font-semibold text-gray-600 ml-5">
+            Radius
+          </label>
+          <select className="w-full px-4 py-2 rounded-full bg-transparent focus:outline-none">
+            <option>0 Miles</option>
+            <option>0-2 Miles</option>
+            <option>2-5 Miles</option>
+            <option>5-10 Miles</option>
+            <option>10-15 Miles</option>
+            <option>15-20 Miles</option>
+          </select>
+        </div>
+        {/* Location Dropdown */}
+        <div className="flex flex-col rounded-full p-2 hover:bg-gray-100 transition-colors duration-200">
+          <label className="text-xs font-semibold text-gray-600 ml-5">
+            Location
+          </label>
+          <select
+
+            className="w-full px-4 py-2 rounded-full bg-transparent focus:outline-none"
+          >
+            <option value="">Any (UK)</option>
+            <option value="london">London</option>
+            <option value="manchester">Manchester</option>
+            <option value="birmingham">Birmingham</option>
+            <option value="edinburgh">Edinburgh</option>
+            <option value="liverpool">Liverpool</option>
+            <option value="glasgow">Glasgow</option>
+          </select>
+        </div>
+        {/* Rate Dropdown */}
+        <div className="flex flex-col rounded-full p-2 hover:bg-gray-100 transition-colors duration-200">
+          <label className="text-xs font-semibold ml-4 text-gray-600">
+            Rate
+          </label>
+          <select
+
+            className="w-full px-4 py-2 rounded-full bg-transparent focus:outline-none"
+          >
+            <option value="">Any</option>
+            <option value="10">£0-50</option>
+            <option value="20">£50-100</option>
+            <option value="50">£100-200</option>
+            <option value="100">£200-500</option>
+            <option value="200">£500+</option>
+          </select>
+        </div>
+        {/* Search Button */}
+        <div className="relative">
+          <button className="p-2 md:p-5 bg-gradient-to-r from-blue-600 via-blue-600 to-blue-400 rounded-full text-white font-semibold hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
+            <FaSearch />
+          </button>
+        </div>
+      </div>
+      </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {currentJobs.map((job, index) => (
